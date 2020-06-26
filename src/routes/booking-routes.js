c6d0c3dabad436f392699083646d80fc0e551df1
Saksham Kumar <sakshamk88@gmail.com>
2020-06-26 17:49:11 +0530

    bug removed

--- src/routes/booking-routes.js
+++ src/routes/booking-routes.js
@@ -4,20 +4,19 @@ const Bookings = require("../models/bookings");
 const bookingrouter = new express.Router();
 
 //make a booking
-bookingrouter.post("/book", (req, res) => {
-    const session = req.session;
-    if (!session.user) {
-      res.status(401).send("You are not authenticated.");
-    }
-    try{  
-        const newBooking = new Bookings(req.body);
-        await newBooking.save();
-        res.status(200).send("Booking Made")
-    }
-    catch(err) {
-        res.status(500).send(err);
-    }
-})
+bookingrouter.post("/book", async (req, res) => {
+  const session = req.session;
+  if (!session.user) {
+    res.status(401).send("You are not authenticated.");
+  }
+  try {
+    const newBooking = new Bookings(req.body);
+    await newBooking.save();
+    res.status(200).send("Booking Made");
+  } catch (err) {
+    res.status(500).send(err);
+  }
+});
 
 //get all the bookings of a particular owner(admin)
 bookingrouter.get("", async (req, res) => {
@@ -25,21 +24,20 @@ bookingrouter.get("", async (req, res) => {
   if (!session.user) {
     res.status(401).send("You are not authenticated.");
   }
-  if(!(session.user.role === 'admin')){
+  if (!(session.user.role === "admin")) {
     res.status(401).send("You are not authorised.");
   }
 
   //const allbookings = await Bookings.find().gt("date", new Date());
-  try{
-  const allBookings = await Bookings.findAll({owner: session.user.userId});
-  const currDate = new Date();
-  const bookings = await allBookings.filter((booking) => {
-    return booking.date >= currDate;
-  })
-  res.status(200).send(bookings);
-  ;}
-  catch(err){
-    res.status(500).send({error: err});
+  try {
+    const allBookings = await Bookings.findAll({ owner: session.user.userId });
+    const currDate = new Date();
+    const bookings = await allBookings.filter((booking) => {
+      return booking.date >= currDate;
+    });
+    res.status(200).send(bookings);
+  } catch (err) {
+    res.status(500).send({ error: err });
   }
 });
 
@@ -54,11 +52,11 @@ bookingrouter.post("/user", async (req, res) => {
   }
 
   try {
-    const currDate  = new Date();
-    const userBookings = await Bookings.findAll({ client : req.body.userId });
+    const currDate = new Date();
+    const userBookings = await Bookings.findAll({ client: req.body.userId });
     const bookings = await userBookings.map((booking) => {
-        return booking.date >= currDate;
-    })
+      return booking.date >= currDate;
+    });
     if (!bookings) {
       throw new Error("Can't find any bookings for this user.");
     }
