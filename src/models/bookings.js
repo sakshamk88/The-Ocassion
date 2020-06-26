e01a5d20682f1fec6cbd88517314c60209c7443e
Saksham Kumar <sakshamk88@gmail.com>
2020-06-26 18:29:21 +0530

    server config updated

--- src/models/bookings.js
+++ src/models/bookings.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
-const validator = require("validatore");
+const validator = require("validator");
 
 const bookingSchema = new mongoose.Schema({
   propertyId: {
--- src/server.js
+++ src/server.js
@@ -20,9 +20,25 @@ app.use(cookieParser());
 
 app.use(
   cors({
-    origin: "*",
+    origin: "http://localhost:3001",
+    credentials: true,
   })
 );
+
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Credentials", true);
+  res.header("Access-Control-Allow-Origin", req.headers.origin);
+  res.header(
+    "Access-Control-Allow-Methods",
+    "GET,PUT,POST,DELETE,UPDATE,OPTIONS"
+  );
+  res.header(
+    "Access-Control-Allow-Headers",
+    "X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept"
+  );
+  next();
+});
+
 app.use(express.static(publicDirectoryPath));
 const MongoStore = connectStore(session);
 
